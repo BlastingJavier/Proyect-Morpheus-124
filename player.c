6c54c768f2e4e29dd7ff01806fdb413e8851e66b
BlastingJavier <31637787+BlastingJavier@users.noreply.github.com>
2018-02-20 02:09:56 +0100

    Update player.c

--- player.c
+++ player.c
@@ -13,38 +13,51 @@
 #include <stdlib.h>
 #include <string.h>
 #include "types.h"
+#include "object.h"
+#include "space.h"
 #include "player.h"
 
+
+/*Estrucura que para las caracteristicas de player*/
 struct _Player {
-  Id id;
+  Id player_id;
   char name[WORD_SIZE+1];
-  Id inventory_item;/*objeto del inventario*/
-  Id location;/*localicación*/
+  Id space_id;/*objeto del inventario*/
+  Id inventory_item_id;/*localicación*/
 };
 
-/**
-PLAYER CREATE : CONSTRUCTOR : si player == 0 retornara ERROR  guardara memoria dinamica
-y asigna valores a los campos de la estructura nueva creada (newPlayer) retorna el puntero a estructura
-*/
+
+
+/*
+ * @brief Se encarga de crear el jugador,
+   y poner el caracter fin de cadena al final del nombre de este
+ * @param id, de tipo Id
+ * @return newPlayer, que es el puntero a la estructura
+ */
 Player* player_create (Id id){
   Player *newPlayer = NULL;
   if (id == NO_ID){
-    return NULL
+    return NULL;
   }
-  newPlayer = (Player*)malloc(sizeof(Player))
+  newPlayer = (Player*)malloc(sizeof(Player));
 
   if (newPlayer==NULL){
     return NULL;
   }
-  newPlayer->id
+  newPlayer->player_id=id;
   newPlayer->name[0] = '\0';
+  newPlayer->inventory_item_id=NO_ID;
 
   return newPlayer;
 }
-/**
-PLAYER CREATE : DESTRUCTOR : si player == 0 retornara ERROR  liberara memoria y si lo llega a hacer
-devolvera un codigo de estado (OK);
-*/
+
+
+
+/*
+ * @brief Libera memoria para player.
+ * @param Player: puntero a Player .
+ * @return status OK o ERROR.
+ */
 STATUS player_destroy (Player* player){
   if(!player){
     return ERROR;
@@ -55,10 +68,16 @@ STATUS player_destroy (Player* player){
 
   return OK;
 }
-/**
-PLAYER SET NAME: Esta función simplemente establece el nombre copiándolo de player a (*player).name, en el caso de que el destino sea NULL, devuelve Error con el condicional
-*/
-STATUS player_set_name (Player* , char* name){
+
+
+
+/*
+ * @brief Pone o cambia el nombre del jugador
+ * @param objeto: puntero a Objeto.
+ * @param name: puntero a char.
+ * @return status OK o ERROR.
+ */
+STATUS player_set_name (Player* player, char* name){
   if (!player || !name){
     return ERROR;
   }
@@ -68,76 +87,119 @@ STATUS player_set_name (Player* , char* name){
 
   return OK;
 }
-/**
-PLAYER GET NAME: Si el player == 0 retorna NULL si no retorna el nombre del player (Player->player)
-*/
+
+
+
+/*
+ * @brief Pone o cambia el objeto del inventario
+ * @param player: puntero a Player.
+ * @param objeto: campo de Objeto.
+ * @return status OK o ERROR.
+ */
+STATUS player_set_object(Player* player,Id object) {
+  if (!player || !object) {
+    return ERROR;
+  }
+  player->inventory_item_id = object;
+  return OK;
+}
+
+
+
+/*
+ * @brief Pone o cambia la localizacion de player
+ * @param player: puntero a Player.
+ * @param location: campo de Space.
+ * @return status OK o ERROR.
+ */
+STATUS player_set_location(Player* player,Id location) {
+  if (!player || !location) {
+    return ERROR;
+  }
+  player->space_id = location;
+  return OK;
+}
+
+
+
+/*
+ * @brief Devuelve el nombre asignado a un jugador
+ * @param jugador: puntero a Jugador.
+ * @return name, el nombre que asignamos al jugador
+ */
 const char * player_get_name(Player* player) {
   if (!player) {
     return NULL;
   }
   return player->name;
 }
-/**
-PLAYER SET INVENTORY ITEM: Si el player es NULL o id == (-1) habrá un error , si no , al player arma
-se le asignará el valor de id
-*/
-STATUS player_set_inventory_item(Player* player, Id id) {
-  if (!player || id == NO_ID) {
-    return ERROR;
-  }
-  player->inventory_item = id;
-  return OK;
-}
-/**
-PLAYER SET LOCATION: Si el player es NULL o id == (-1) habrá un error , si no , al player util
-se le asignará el valor de id
-*/
-STATUS player_set_location(Player* player, Id id) {
-  if (!player || id == NO_ID) {
-    return ERROR;
-  }
-  player->location = id;
-  return OK;
-}
-/**
-PLAYER GET INVENTORY ITEM: Si el player == 0 retorna (-1) si no retorna el id del player (Player->id)
-*/
+
+
+
+/*
+ * @brief Devuelve la inventory_item
+ * @param jugador: puntero a Jugador.
+ * @return player->inventory_item (objeto)
+ */
 Id player_get_inventory_item(Player* player) {
   if (!player) {
     return NO_ID;
   }
-  return player->inventory_item;
+  return player->inventory_item_id;
 }
-/**
-PLAYER GET LOCATION: Si el player == 0 retorna (-1) si no retorna el id del player (Player->id)
-*/
+
+
+
+/*
+ * @brief Devuelve la location
+ * @param jugador: puntero a Jugador.
+ * @return player->location(localizacion)
+ */
 Id player_get_location(Player* player) {
   if (!player) {
     return NO_ID;
   }
-  return player->location;
+  return player->space_id;
 }
-/**
-PLAYER PRINT: Control de errores Debugging
-*/
-STATUS player_print(Player* player) {
-  Id idaux = NO_ID;
 
+
+
+/*
+ * @brief Devuelve el id del jugador
+ * @param jugador: puntero a Jugador.
+ * @return player->player_id(identificador)
+ */
+Id player_get_id(Player * player){
+  if (!player){
+    return NO_ID;
+  }
+  return player->player_id;
+}
+
+
+
+/*
+ * @brief Muestra por la pantalla de salida, tanto el id, como el nombre del jugador
+ * @param jugador: puntero a Jugador.
+ * @return status, OK o ERROR
+ */
+ /**
+ * USO ALTERNATIVO ==> PLAYER PRINT: Control de errores Debugging
+ */
+STATUS player_print(Player* player) {
   if (!player) {
     return ERROR;
   }
 
-  fprintf(stdout, "--> Player (Id: %ld; Name: %s)\n", player->id, player->name);
+  fprintf(stdout, "--> Player (Id: %ld; Name: %s)\n", player->player_id, player->name);
 
-  idaux = player_get_inventory_item(player);
-  if (NO_ID != idaux) {
-    fprintf(stdout, "---> inventory_item: %ld.\n", idaux);
+  if (NO_ID != player_get_inventory_item(player)) {
+    fprintf(stdout, "---> inventory_item: %ld.\n",(long)player_get_inventory_item(player));
   }else {
     fprintf(stdout, "---> No inventory_item.\n");
   }
-  idaux = player_get_location(player);
-  if (NO_ID != idaux) {
-    fprintf(stdout, "---> Location: %ld.\n", idaux);
+  if (NO_ID != player_get_location(player)) {
+    fprintf(stdout, "---> Location: %ld.\n",(long)player_get_location(player));
   }else {
     fprintf(stdout, "---> No location.\n");
   }
