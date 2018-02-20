2ad88ea9eb752df72ee038d88a6976684153b179
BlastingJavier <31637787+BlastingJavier@users.noreply.github.com>
2018-02-20 02:08:51 +0100

    Update object.c

--- object.c
+++ object.c
@@ -15,36 +15,47 @@
 #include "types.h"
 #include "object.h"
 
+/*Estructura que define un objeto(características) */
 struct _Object {
   Id id;
   char name[WORD_SIZE+1];
-  Id object_weapon;/*armas*/
-  Id object_tool;/*utiles*/
-  Id object_key;/*llaves*/
+  Id object_weapon;/*armas:Utilidad posterior*/
+  Id object_tool;/*utiles:Utilidad posterior*/
+  Id object_key;/*llaves:Utilidad posterior*/
+  Id location;
 };
-/**
-OBJECT CREATE : CONSTRUCTOR : si objeto == 0 retornara ERROR  guardara memoria dinamica
-y asigna valores a los campos de la estructura nueva creada (newObject) retorna el puntero a estructura
-*/
+
+
+
+/*
+ * @brief Se encarga de crear el objeto,
+   y poner el caracter fin de cadena al final del nombre de este
+ * @param id, de tipo Id
+ * @return newObject, que es el puntero a la estructura
+ */
 Object* object_create (Id id){
   Object *newObject = NULL;
   if (id == NO_ID){
-    return NULL
+    return NULL;
   }
-  newObject = (Object*)malloc(sizeof(Object))
+  newObject = (Object*)malloc(sizeof(Object));
 
   if (newObject==NULL){
     return NULL;
   }
-  newObject->id
-  newObject->name[0] = '\0';
+  newObject->id=id;
+  newObject->name[0]='\0';
 
   return newObject;
 }
-/**
-OBJECT CREATE : DESTRUCTOR : si objeto == 0 retornara ERROR  liberara memoria y si lo llega a hacer
-devolvera un codigo de estado (OK);
-*/
+
+
+
+/*
+ * @brief Libera memoria para objeto.
+ * @param objeto: puntero a Objeto.
+ * @return status OK o ERROR.
+ */
 STATUS object_destroy (Object* object){
   if(!object){
     return ERROR;
@@ -55,10 +66,167 @@ STATUS object_destroy (Object* object){
 
   return OK;
 }
-/**
-OBJECT SET NAME: Esta función simplemente establece el nombre copiándolo de objeto a (*objeto).name, en el caso de que el destino sea NULL, devuelve Error con el condicional
-*/
-STATUS object_set_name (Object* , char* name){
+
+
+/*
+ * @brief Devuelve el id de un objeto
+ * @param objeto: puntero a Objeto.
+ * @return Id NO_ID o puntero (object->id).
+ */
+Id object_get_id(Object *object){
+  if (object==NULL){
+    return NO_ID;
+  }
+  else {
+    return object->id;
+  }
+}
+
+
+
+/*
+ * @brief Devuelve el nombre asignado a un objeto
+ * @param objeto: puntero a Objeto.
+ * @return name, el nombre que asignamos al objeto
+ */
+const char * object_get_name(Object* object) {
+  if (!object) {
+    return NULL;
+  }
+  return object->name;
+}
+
+
+
+/*
+ * @brief Devuelve el id asignado a un objeto
+ * @param objeto: puntero a Objeto.
+ * @return location, campo object->location
+ */
+Id object_get_location(Object* object){
+  if (object == NULL){
+    return NO_ID;
+  }
+  else {
+    return object->location;
+  }
+}
+
+
+
+/*
+ * @brief Devuelve el arma
+ * @param objeto: puntero a Objeto.
+ * @return arma, el arma
+ */
+Id object_get_weapon(Object* objeto) {
+  if (!objeto) {
+    return NO_ID;
+  }
+  return objeto->object_weapon;
+}
+
+
+
+/*
+ * @brief Devuelve la herramienta
+ * @param objeto: puntero a Objeto.
+ * @return tool, el nombre que asignamos a la herramienta
+ */
+Id object_get_tool(Object* objeto) {
+  if (!objeto) {
+    return NO_ID;
+  }
+  return objeto->object_tool;
+}
+
+
+
+/*
+ * @brief Devuelve la llave
+ * @param objeto: puntero a Objeto.
+ * @return llave, la llave
+ */
+Id object_get_key(Object* objeto) {
+  if (!objeto) {
+    return NO_ID;
+  }
+  return objeto->object_key;
+}
+
+
+
+/*
+ * @brief Pone o cambia el arma
+ * @param objeto: puntero a Objeto.
+ * @param id: del type Id
+ * @return status OK o ERROR.
+ */
+STATUS object_set_weapon(Object* object, Id id) {
+  if (!object || id == NO_ID) {
+    return ERROR;
+  }
+  object->object_weapon = id;
+  return OK;
+}
+
+
+
+/*
+ * @brief Pone o cambia el id del objeto conforme a la location
+ * @param objeto: puntero a Objeto.
+ * @param id: del type Id
+ * @return status OK o ERROR.
+ */
+STATUS object_set_location(Object* object , Id id){
+  if (!object || id == NO_ID){
+    return ERROR;
+  }
+  object->location = id;
+  return OK;
+}
+
+
+
+/*
+ * @brief Pone o cambia el tool
+ * @param objeto: puntero a Objeto.
+ * @param id: del type Id
+ * @return status OK o ERROR.
+ */
+STATUS object_set_tool(Object* object, Id id) {
+  if (!object || id == NO_ID) {
+    return ERROR;
+  }
+  object->object_tool = id;
+  return OK;
+}
+
+
+
+/*
+ * @brief Pone o cambia la llave
+ * @param objeto: puntero a Objeto.
+ * @param id: del type Id
+ * @return status OK o ERROR.
+ */
+STATUS object_set_key(Object* object, Id id) {
+  if (!object || id == NO_ID) {
+    return ERROR;
+  }
+  object->object_tool = id;
+  return OK;
+}
+
+
+
+/*
+ * @brief Pone o cambia el nombre del objeto
+ * @param objeto: puntero a Objeto.
+ * @param name: puntero a char.
+ * @return status OK o ERROR.
+ */
+STATUS object_set_name (Object* object, char* name){
   if (!object || !name){
     return ERROR;
   }
@@ -68,100 +236,22 @@ STATUS object_set_name (Object* , char* name){
 
   return OK;
 }
-/**
-OBJECT GET NAME: Si el objeto == 0 retorna NULL si no retorna el nombre del objeto (Object->objeto)
-*/
-const char * object_get_name(Object* object) {
-  if (!object) {
-    return NULL;
-  }
-  return object->name;
-}
-/**
-OBJECT SET NAME: Si el objeto == 0 retorna NULL si no retorna el nombre del objeto (Object->objeto)
-*/
-STATUS object_set_name(Object* object, char* name) {
-  if (!object || !name) {
-    return ERROR;
-  }
 
-  if (!strcpy(object->name, name)) {
-    return ERROR;
-  }
 
-  return OK;
-}
-/**
-OBJECT SET WEAPON: Si el objeto es NULL o id == (-1) habrá un error , si no , al objeto arma
-se le asignará el valor de id
-*/
-STATUS object_set_weapon(Object* object, Id id) {
-  if (!object || id == NO_ID) {
-    return ERROR;
-  }
-  object->object_weapon = id;
-  return OK;
-}
-/**
-OBJECT SET TOOL: Si el objeto es NULL o id == (-1) habrá un error , si no , al objeto util
-se le asignará el valor de id
-*/
-STATUS object_set_tool(Object* object, Id id) {
-  if (!object || id == NO_ID) {
-    return ERROR;
-  }
-  object->object_tool = id;
-  return OK;
-}
-/**
-OBJECT SET KEY: Si el objeto es NULL o id == (-1) habrá un error , si no , al objeto llave
-se le asignará el valor de id
-*/
-STATUS object_set_key(Object* object, Id id) {
-  if (!object || id == NO_ID) {
-    return ERROR;
-  }
-  object->object_tool = id;
-  return OK;
-}
-/**
-OBJECT GET WEAPON: Si el objeto == 0 retorna (-1) si no retorna el id del objeto (Object->id)
-*/
-Id object_get_weapon(Object* objeto) {
-  if (!objeto) {
-    return NO_ID;
-  }
-  return objeto->object_weapon;
-}
-/**
-OBJECT GET TOOL: Si el objeto == 0 retorna (-1) si no retorna el id del objeto (Object->id)
-*/
-Id object_get_tool(Object* objeto) {
-  if (!objeto) {
-    return NO_ID;
-  }
-  return objeto->object_tool;
-}
-/**
-OBJECT GET KEY: Si el objeto == 0 retorna (-1) si no retorna el id del objeto (Object->id)
-*/
-Id object_get_key(Object* objeto) {
-  if (!objeto) {
-    return NO_ID;
-  }
-  return objeto->object_key;
-}
-/**
-OBJECT PRINT: Control de errores Debugging
-*/
+
+/*
+ * @brief Muestra por la pantalla de salida, tanto el id, como el nombre del objeto
+ * @param objeto: puntero a Objeto.
+ * @return status, OK o ERROR
+ */
 STATUS object_print(Object* object) {
   Id idaux = NO_ID;
 
-  if (!objeto) {
+  if (!object) {
     return ERROR;
   }
 
-  fprintf(stdout, "--> Object (Id: %ld; Name: %s)\n", objeto->id, objeto->name);
+  fprintf(stdout, "--> Object (Id: %ld; Name: %s)\n", object->id, object->name);
 
   idaux = object_get_weapon(object);
   if (NO_ID != idaux) {
