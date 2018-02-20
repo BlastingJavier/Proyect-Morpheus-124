6b2d80f6e74b8b49b5340e708449907ea8bd04c5
BlastingJavier <31637787+BlastingJavier@users.noreply.github.com>
2018-02-20 02:12:44 +0100

    Update space.c

--- space.c
+++ space.c
@@ -1,9 +1,21 @@
+/**
+*@brief It defines a space
+*
+*
+*@file space.c
+*@author FJNR & AMH
+*@version 1.0
+*@date 05/02/2018
+*@copyright GNU Public License
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "types.h"
 #include "space.h"
 
+/*Estructura que define un espacio (características)*/
 struct _Space {
   Id id;
   char name[WORD_SIZE + 1];
@@ -11,15 +23,40 @@ struct _Space {
   Id south;
   Id east;
   Id west;
-  BOOL object;
+  Id object;
 };
+/**                 Definidos en:
+                        ||
+                        ||
+                        V
+WORD_SIZE = 1000 <==types.h
+MAX_SPACES = 100 <==space.h
+FIRST_SPACE = 1 <==space.h
+NO_ID = -1 <==types.h
+NO_CMD = -1 <==command.h
+UNKNOWN = 0 <==command.h
+EXIT = 1 <==command.h
+FOLLOWING = 2 <==command.h
+PREVIOUS = 3 <==command.h
+SCREEN_MAX_STR = 80 <==screen.h
+P.F.: Private Function
+*/
 
+
+
+/*
+ * @brief Se encarga de iniciar la estructura de espacio
+   y poner el caracter fin de cadena al final del nombre de este
+ * @param id, de tipo Id
+ * @return newSpace, que es el puntero a la estructura Space
+ */
 Space* space_create(Id id) {
 
   Space *newSpace = NULL;
 
-  if (id == NO_ID)
+  if (id == NO_ID){
     return NULL;
+  }
 
   newSpace = (Space *) malloc(sizeof (Space));
 
@@ -35,11 +72,18 @@ Space* space_create(Id id) {
   newSpace->east = NO_ID;
   newSpace->west = NO_ID;
 
-  newSpace->object = FALSE;
+  newSpace->object = NO_ID;
 
   return newSpace;
 }
 
+
+
+/*
+ * @brief Libera memoria para space
+ * @param space: puntero a Space.
+ * @return status OK o ERROR.
+ */
 STATUS space_destroy(Space* space) {
   if (!space) {
     return ERROR;
@@ -51,6 +95,14 @@ STATUS space_destroy(Space* space) {
   return OK;
 }
 
+
+
+/*
+ * @brief Pone o cambia el nombre del espacio
+ * @param space: puntero a Space.
+ * @param name: puntero a char.
+ * @return status OK o ERROR.
+ */
 STATUS space_set_name(Space* space, char* name) {
   if (!space || !name) {
     return ERROR;
@@ -63,6 +115,14 @@ STATUS space_set_name(Space* space, char* name) {
   return OK;
 }
 
+
+
+/*
+ * @brief Pone o cambia el north
+ * @param space: puntero a Space
+ * @param id: campo de Id
+ * @return status OK o ERROR o NO_ID
+ */
 STATUS space_set_north(Space* space, Id id) {
   if (!space || id == NO_ID) {
     return ERROR;
@@ -71,6 +131,14 @@ STATUS space_set_north(Space* space, Id id) {
   return OK;
 }
 
+
+
+/*
+ * @brief Pone o cambia el south
+ * @param space: puntero a Space.
+ * @param id: del type Id
+ * @return status OK o ERROR o NO_ID
+ */
 STATUS space_set_south(Space* space, Id id) {
   if (!space || id == NO_ID) {
     return ERROR;
@@ -79,6 +147,14 @@ STATUS space_set_south(Space* space, Id id) {
   return OK;
 }
 
+
+
+/*
+ * @brief Pone o cambia el east
+ * @param space: puntero a Space.
+ * @param name: puntero a char.
+ * @return status OK o ERROR o NO_ID
+ */
 STATUS space_set_east(Space* space, Id id) {
   if (!space || id == NO_ID) {
     return ERROR;
@@ -87,6 +163,14 @@ STATUS space_set_east(Space* space, Id id) {
   return OK;
 }
 
+
+
+/*
+ * @brief Pone o cambia el west
+ * @param space: puntero a Space.
+ * @param id: del type Id
+ * @return status OK o ERROR o NO_ID
+ */
 STATUS space_set_west(Space* space, Id id) {
   if (!space || id == NO_ID) {
     return ERROR;
@@ -95,7 +179,15 @@ STATUS space_set_west(Space* space, Id id) {
   return OK;
 }
 
-STATUS space_set_object(Space* space, BOOL value) {
+
+
+/*
+ * @brief Pone o cambia el espacio del objeto(casilla)
+ * @param space: puntero a Space.
+ * @param value: Id (identificador)
+ * @return status OK o ERROR.
+ */
+STATUS space_set_object(Space* space, Id value) {
   if (!space) {
     return ERROR;
   }
@@ -103,6 +195,13 @@ STATUS space_set_object(Space* space, BOOL value) {
   return OK;
 }
 
+
+
+/*
+ * @brief Devuelve el nombre (casilla)
+ * @param space: puntero a Space.
+ * @return name, el name space->name o NULL
+ */
 const char * space_get_name(Space* space) {
   if (!space) {
     return NULL;
@@ -110,6 +209,13 @@ const char * space_get_name(Space* space) {
   return space->name;
 }
 
+
+
+/*
+ * @brief Devuelve el id (casilla)
+ * @param space: puntero a Space.
+ * @return id, el id space->id o NO_ID
+ */
 Id space_get_id(Space* space) {
   if (!space) {
     return NO_ID;
@@ -117,6 +223,13 @@ Id space_get_id(Space* space) {
   return space->id;
 }
 
+
+
+/*
+ * @brief Devuelve el id de la casilla del norte
+ * @param space: puntero a Space.
+ * @return norte, space->north o NO_ID
+ */
 Id space_get_north(Space* space) {
   if (!space) {
     return NO_ID;
@@ -124,6 +237,13 @@ Id space_get_north(Space* space) {
   return space->north;
 }
 
+
+
+/*
+ * @brief Devuelve el id de la casilla del sur
+ * @param space: puntero a Space.
+ * @return south, space->south o NO_ID
+ */
 Id space_get_south(Space* space) {
   if (!space) {
     return NO_ID;
@@ -131,6 +251,13 @@ Id space_get_south(Space* space) {
   return space->south;
 }
 
+
+
+/*
+ * @brief Devuelve el id de la casilla del norte
+ * @param space: puntero a Space.
+ * @return norte, space->north o NO_ID
+ */
 Id space_get_east(Space* space) {
   if (!space) {
     return NO_ID;
@@ -138,6 +265,13 @@ Id space_get_east(Space* space) {
   return space->east;
 }
 
+
+
+/*
+ * @brief Devuelve el id de la casilla del oeste
+ * @param space: puntero a Space.
+ * @return west, space->west o NO_ID
+ */
 Id space_get_west(Space* space) {
   if (!space) {
     return NO_ID;
@@ -145,13 +279,27 @@ Id space_get_west(Space* space) {
   return space->west;
 }
 
-BOOL space_get_object(Space* space) {
+
+
+/*
+ * @brief Devuelve el si hay o no objeto en la casilla
+ * @param space: puntero a Space.
+ * @return object, space->object o FALSE
+ */
+Id space_get_object(Space* space) {
   if (!space) {
-    return FALSE;
+    return NO_ID;
   }
   return space->object;
 }
 
+
+
+/*
+ * @brief Muestra por la pantalla de salida, tanto el id, como el nombre del espacio
+ * @param space: puntero a Space.
+ * @return status, OK o ERROR o NO_ID
+ */
 STATUS space_print(Space* space) {
   Id idaux = NO_ID;
 
@@ -197,4 +345,3 @@ STATUS space_print(Space* space) {
 
   return OK;
 }
-
